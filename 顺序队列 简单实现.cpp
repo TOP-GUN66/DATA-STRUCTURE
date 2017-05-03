02559d99548865b967142e711573e5d5915bcc7a
TOP-GUN66 <898803540@qq.com>
2017-05-03 09:31:44 +0800

    顺序队列
    一维数组
    入队 出队

---
+++ 顺序队列 简单实现.cpp
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+#define  ElemType int
+#define MAX 10
+typedef struct
+{
+	ElemType data[MAX];
+	int front;
+	int rear;
+
+}SeqQueue;
+
+void EnterQueue(SeqQueue *Q, ElemType e)
+{
+	if ((*Q).rear == MAX - 1)
+	{
+		printf("overflow\n");
+		exit(0);
+	}
+	(*Q).rear++;
+	(*Q).data[(*Q).rear] = e;
+
+}
+void DeleteQueue(SeqQueue *Q)
+{
+	
+	if ((*Q).front == (*Q).rear)
+	{
+		printf("Empty\n");
+		exit(0);
+		
+	}
+	
+	(*Q).front++;
+
+
+}
+
+int main(void)
+{
+	SeqQueue Q;
+	Q.data[MAX] = { -1};
+	Q.front = -1;
+	Q.rear = -1;
+	int a, b;
+	ElemType c;
+	printf("Input data:");
+	scanf_s("%d", &a);
+	EnterQueue(&Q, a);
+	printf("Input data:");
+	scanf_s("%d", &b);
+	EnterQueue(&Q, b);
+
+	DeleteQueue(&Q);
+	
+	printf("队首位置%d\n", Q.front);
+	printf("队尾位置%d\n", Q.rear);
+
+
+	
+	system("pause");
+    return 0;
+}
